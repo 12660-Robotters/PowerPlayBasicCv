9ea5ae5332ac0d82c785bff711c5e5bf507434b2
foehammer127 <44120040+foehammer127-svg@users.noreply.github.com>
2022-10-12 13:11:28 -0500

    Initial Commit

---
+++ PowerplayBasicCv.java
@@ -0,0 +1,92 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+
+import java.util.List;
+
+public class PowerplayBasicCv {
+    // Make this the actual file
+    private ClassFactory classFactory;
+    private static final String TFOD_MODEL_ASSET = "PowerPlay.tflite";
+    // These are the Labels, you can change this to use your custom model from the ML Toolchain if you want!
+    private final String[] labels = {
+            "1 Bolt",
+            "2 Bulb",
+            "3 Panel",
+    };
+
+    private HardwareMap hardwareMap;
+
+    // Declare the Objects
+    private VuforiaLocalizer vuforia;
+
+    private TFObjectDetector tfod;
+
+    public PowerplayBasicCv() {}
+
+    public void init(HardwareMap hardwareMap) {
+        initVuforia();
+        initTfod();
+
+        if(tfod != null) {
+            tfod.activate();
+
+            tfod.setZoom(1.0, 16.0/9.0);
+        }
+
+
+
+    }
+
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+
+        TFObjectDetector.Parameters tfodParamaters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+
+        // Result Confidence for Tuning
+        tfodParamaters.minResultConfidence = 0.75f;
+        tfodParamaters.isModelTensorFlow2 = true;
+        tfodParamaters.inputSize = 300;
+
+        tfod = classFactory.createTFObjectDetector(tfodParamaters, vuforia);
+
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, labels);
+    }
+    private void initVuforia() {
+        // Start Seting up vuforia
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+        // Set this to your vuforia api key
+        String VUFORIA_KEY = "1234";
+        // Change this to Vuforia License Key
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        // Set Webcam Name to webcam in config or modify this variable
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "webcam");
+        vuforia =classFactory.createVuforia(parameters);
+    }
+
+
+    // Returns Parking Spot 1,2,3 or -1 if not detected
+    public int pullcv() {
+        List<Recognition> recognitionList = tfod.getUpdatedRecognitions();
+        if(recognitionList != null) {
+            for (Recognition rec : recognitionList) {
+                for(String item : labels) {
+                    if (rec.getLabel().equals(item)) {
+                        String[] items = item.split(" ");
+                        int returned = Integer.parseInt(items[0]);
+                        return returned;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+# A Basic CV Object Declaration Using the Base FTC Model For The Power Play(2022-2023) Season:
+
+## Installation:
+
+
+### Android Studio:
+
+Paste into the Teamcode Folder and follow Usage
+
+
+### OnBotJava/Blocks:
+
+Paste into the Teamcode Folder and Follow Usage
+
+
+## Usage:
+
+### Java:
+
+First, create an Op Mode, and add this line to the top of the class file
+
+    private PowerPlayBasicCv cvClass = new PowerPlayEasyCv();
+
+Where you run your code add this line before the waitforstart command, making sure to pass your hardware map as a paramater. 
+
+    cvClass.init(hardwareMap);
+
+Finally Use 
+    
+    int parkingspot = cvClass.pullcv();
+
+then use parkingspot in your code. 
